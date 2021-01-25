15eea80f9933d84fdcff892423ca92ad26b87ac6
ryudaibaba <1844ryudai@gmail.com>
2021-01-25 23:01:21 +0900

    #1

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // シードデータの追加
+
+        // 社員
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10) . '@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => '社員',
+        ]);
+
+        // システム開発者
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10) . '@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => '開発者',
+        ]);
+        
+        // 管理者（社長）
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10) . '@gamil.com',
+            'password' => Hash::make('baseball'),
+            'role' => '管理者',
+        ]);
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,25 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// 全ユーザーはログインとログアウト、パスワードの変更ができる
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('password/confirm ', 'Auth\ConfirmPasswordController@showConfirmForm')->name('password.confirm');
+Route::post('password/confirm', 'Auth\ConfirmPasswordController@confirm');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+
+// 管理者と開発者のみ許可のルート
+Route::middleware(['auth', 'can:system-higher'])->group(function () {
+    Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+    Route::post('register', 'Auth\RegisterController@register');
+});
+
